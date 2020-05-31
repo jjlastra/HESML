8c9c499030241a8a58e3ee2a24e6c126c60ec99c
j.lastra <jlastra@invi.uned.es>
2020-05-31 23:40:30 +0200

    IC-based vertex comparator

---
+++ HESML_Library/HESML/src/hesml/taxonomy/impl/IcVertexComparator.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright (C) 2016-2020 Universidad Nacional de Educación a Distancia (UNED)
+ * 
+ * This program is free software for non-commercial use:
+ * you can redistribute it and/or modify it under the terms of the
+ * Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International
+ * (CC BY-NC-SA 4.0) as published by the Creative Commons Corporation,
+ * either version 4 of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * section 5 of the CC BY-NC-SA 4.0 License for more details.
+ * 
+ * You should have received a copy of the Creative Commons
+ * Attribution-NonCommercial-ShareAlike 4.0 International (CC BY-NC-SA 4.0) 
+ * license along with this program. If not,
+ * see <http://creativecommons.org/licenses/by-nc-sa/4.0/>.
+ */
+package hesml.taxonomy.impl;
+
+import hesml.taxonomy.IVertex;
+import java.util.Comparator;
+
+/**
+ * Comparator of vertexex according to their IC value
+ * @author juan
+ */
+
+class IcVertexComparator implements Comparator<IVertex>
+{
+    /**
+     * This function compare the IC values of two vertexes
+     * @param a
+     * @param b
+     * @return 
+     */
+
+    @Override
+    public int compare(IVertex a, IVertex b)
+    {
+        return ((int)Math.signum(b.getICvalue() - a.getICvalue()));
+    }
+}
