0e3d5bc5a6ae77db8170659541bfe07eaf9158dc
root <alicialaraclares@gmail.com>
2021-11-29 15:20:33 +0100

    Update the results for HESMLSTS experiments

--- HESML_Library/ReproducibleExperiments/Post-scripts/bio_sentence_sim_allExperiments_table.R
+++ HESML_Library/ReproducibleExperiments/Post-scripts/bio_sentence_sim_allExperiments_table.R
@@ -127,9 +127,11 @@ source(paste("bio_sentence_sim_scripts", "readNERexperiment.R", sep = "/"), loca
 
 # We add the experiments to the list
 
-rawdata_experiments <- list(rawdata_string, rawdata_OurWE, rawdata_WBSM, rawdata_UBSM, rawdata_COM, rawdata_COMMixed, rawdata_SWEM, rawdata_BERT, rawdata_Sent2Vec, rawdata_USE, rawdata_Flair, rawdata_BESTCOMBS, rawdata_NERexperiment)
+# rawdata_experiments <- list(rawdata_string, rawdata_OurWE, rawdata_WBSM, rawdata_UBSM, rawdata_COM, rawdata_COMMixed, rawdata_SWEM, rawdata_BERT, rawdata_Sent2Vec, rawdata_USE, rawdata_Flair)
 
 # rawdata_experiments <- list(rawdata_BESTCOMBS, rawdata_NERexperiment)
+
+rawdata_experiments <- list(rawdata_BESTCOMBS)
 
 # We initialize the counter of executions
 
